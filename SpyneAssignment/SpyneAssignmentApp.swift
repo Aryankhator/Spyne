d26a3213bc93dc5042086b3a744d808bcd8bb74f
Aryan <aryanKhator.ak@gmail.com>
2024-12-08 15:08:38 +0530

    first commit

---
+++ SpyneAssignment/SpyneAssignmentApp.swift
@@ -0,0 +1,19 @@
+//
+//
+// SpyneAssignmentApp.swift
+// SpyneAssignment
+//
+// Created by Aryan on 08/12/24
+//
+        
+
+import SwiftUI
+
+@main
+struct SpyneAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
